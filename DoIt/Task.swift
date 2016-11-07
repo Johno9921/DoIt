6820ef905201ac1bc6855a1cca3df5bff3887535
Jonathan Campbell <Johno9921@gmail.com>
2016-11-06 17:58:47 -0800

    Almost Finished Commit!

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jonathan Campbell on 11/6/16.
+//  Copyright © 2016 Zappy Code. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
